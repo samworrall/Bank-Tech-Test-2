eb91c32d93008f11d5c83ca210a44947ed0c09ff
Sam Worrall <samuel.worrall@hotmail.co.uk>
2018-09-14 12:20:58 +0100

    Update test for balance on initialisation to include constant

--- lib/account.rb
+++ lib/account.rb
@@ -1,7 +1,8 @@
 class Account
+  MINIMUM_BALANCE = 0
   attr_reader :balance
-  
+
   def initialize
-    @balance = 0
+    @balance = MINIMUM_BALANCE
   end
 end
--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -2,6 +2,10 @@ require 'account'
 
 describe Account do
   describe '#balance', :balance do
+    it 'Returns the minimum balance upon initialisation' do
+      expect(subject.balance).to eq(Account::MINIMUM_BALANCE)
+    end
+
     it 'Returns the current balance of the account' do
       expect(subject.balance).to eq(0)
     end
