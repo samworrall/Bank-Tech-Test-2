23a810049627dc9b1cd48ae24752c183aec4f2c1
Sam Worrall <samuel.worrall@hotmail.co.uk>
2018-09-14 13:23:19 +0100

    History transaction_history begins as an empty array

---
+++ lib/history.rb
@@ -0,0 +1,7 @@
+class History
+  attr_reader :transaction_history
+
+  def initialize
+    @transaction_history = []
+  end
+end
--- spec/history_spec.rb
+++ spec/history_spec.rb
@@ -1,1 +1,9 @@
 require 'history'
+
+describe History do
+  describe '#transaction_history', :t_hist do
+    it 'Returns an empty array on initialisation' do
+      expect(subject.transaction_history).to eq([])
+    end
+  end
+end
