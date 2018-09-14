26857a8f7f1b1021bfda3f95a23a0e2c1c43454f
Sam Worrall <samuel.worrall@hotmail.co.uk>
2018-09-14 13:38:24 +0100

    Transaction history is correctly updated after logging

--- spec/history_spec.rb
+++ spec/history_spec.rb
@@ -5,6 +5,11 @@ describe History do
     it 'Returns an empty array on initialisation' do
       expect(subject.transaction_history).to eq([])
     end
+
+    it 'Contains hash elements once they have been added' do
+      subject.log('14/09/2018', 10, 0, 10)
+      expect(subject.transaction_history).to eq([{date: '14/09/2018', credit: 10, debit: 0, balance: 10}])
+    end
   end
 
   describe '#log', :log do
