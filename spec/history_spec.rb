32e20a2317d6d2ef118b8c69c7376cdbd22afddb
Sam Worrall <samuel.worrall@hotmail.co.uk>
2018-09-15 08:36:26 +0100

    Change transaction history test to test behaviour

--- spec/history_spec.rb
+++ spec/history_spec.rb
@@ -7,8 +7,7 @@ describe History do
     end
 
     it 'Contains hash elements once they have been added' do
-      subject.log('14/09/2018', 10, 0, 10)
-      expect(subject.transaction_history).to eq([{date: '14/09/2018', credit: 10, debit: 0, balance: 10}])
+      expect { subject.log('14/09/2018', 10, 0, 10) }.to change{ subject.transaction_history }.by([{date: '14/09/2018', credit: 10, debit: 0, balance: 10}])
     end
   end
 
