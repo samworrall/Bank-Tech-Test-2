62de8fd45b1506d362490388a5a1a3fc18145f12
Sam Worrall <samuel.worrall@hotmail.co.uk>
2018-09-16 09:53:09 +0100

    Feature test for successful deposit and withdrawal effects on the account balance

--- spec/feature_spec.rb
+++ spec/feature_spec.rb
@@ -3,28 +3,35 @@ require 'history'
 require 'printer'
 
 describe 'Bank Account', :feature do
+  before(:each) do
+    @date = Time.now.strftime('%d/%m/%Y')
+    @history = History.new
+    @printer = Printer.new
+    @account = Account.new(@history, @printer)
+  end
+
+  it 'Updates the account balance positively' do
+    expect{ @account.deposit(10) }.to change{ @account.balance }.by(10)
+  end
+
+  it 'Updates the account balance negatively' do
+    @account.deposit(10)
+    expect{ @account.withdraw(10) }.to change{ @account.balance }.by(-10)
+  end
 
   it 'Logs a deposit transaction correctly' do
-    date = Time.now.strftime('%d/%m/%Y')
-    history = History.new
-    printer = Printer.new
-    account = Account.new(history, printer)
-    account.deposit(10)
-    expect(history.transaction_history).to eq(
-      [{date: date, credit: 10, debit: 0, balance: 10}]
+    @account.deposit(10)
+    expect(@history.transaction_history).to eq(
+      [{date: @date, credit: 10, debit: 0, balance: 10}]
     )
   end
 
   it 'Logs a withdrawal transaction as well as a deposit transaction' do
-    date = Time.now.strftime('%d/%m/%Y')
-    history = History.new
-    printer = Printer.new
-    account = Account.new(history, printer)
-    account.deposit(20)
-    account.withdraw(10)
-    expect(history.transaction_history).to eq(
-      [{date: date, credit: 20, debit: 0, balance: 20},
-       {date: date, credit: 0, debit: 10, balance: 10}]
+    @account.deposit(20)
+    @account.withdraw(10)
+    expect(@history.transaction_history).to eq(
+      [{date: @date, credit: 20, debit: 0, balance: 20},
+       {date: @date, credit: 0, debit: 10, balance: 10}]
     )
   end
 end
